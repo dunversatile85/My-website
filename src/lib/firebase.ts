831398f99ca1f00f1695ac222da5a7f3b1729e56
Donald Mitchell <donaldtmitchell777@gmail.com>
2025-08-11 15:47:06 +0000

    Same issue

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,13 @@ import { getAnalytics } from "firebase/analytics";
 import { getMessaging } from "firebase/messaging";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyDt_RRKw9WipU4uOPKHuqZ5aZVuMbn_Mhw",
+  authDomain: "dons-playworld.firebaseapp.com",
+  projectId: "dons-playworld",
+  storageBucket: "dons-playworld.firebasestorage.app",
+  messagingSenderId: "4636726751",
+  appId: "1:4636726751:web:f503c77e281710f74c606a",
+  measurementId: ""
 };
 
 // Initialize Firebase
